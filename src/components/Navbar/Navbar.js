0344447d4ed8b82e7e556f8969c7699120c75a1e
titantooth <ali.rabieeie@gmail.com>
2023-09-05 21:03:22 +0330

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,13 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className='nav'>
+    <div className='nav nav-black'>
         <div  className='nav-content'>
 
-            <img className='nav-logo' src='/netflix logo.png' width={115} height={40} alt=''/>
-            <img className='nav-avatar' src='/avatar.png' width={40} height={40} alt=''/>
+            <img className='nav-logo' src='/netflix logo.png'alt=''/>
+            <img className='nav-avatar' src='/avatar.png' alt=''/>
         </div>
 
-        <h1>this is the navbar</h1>
     </div>
   )
 }
