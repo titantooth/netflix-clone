04ee209c7b0a08325415632b91803bec2123b4e7
titantooth <ali.rabieeie@gmail.com>
2023-10-02 14:25:28 +0330

    black if scrolled

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -6,6 +6,35 @@ const HomeScreen = () => {
   return (
     <div className='homeScreen'>
         <Navbar/>
+        
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
+        <h1>TEST</h1>
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+
 import './Navbar.css'
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
+
+  const transitionNavbar = () => {
+    if (window.scrollY> 100) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavbar)
+  
+    return () => {
+      window.removeEventListener('scroll', transitionNavbar)
+    }
+  }, [])
+  
+
   return (
-    <div className='nav nav-black'>
+    <div className={`nav ${show && "nav-black"}`}>
         <div  className='nav-content'>
 
             <img className='nav-logo' src='/netflix logo.png'alt=''/>
