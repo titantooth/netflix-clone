6c347fa333a1e1a9c1fc69c0fe212b74b706677a
titantooth <ali.rabieeie@gmail.com>
2023-10-09 21:09:19 +0330

    asd

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from './axios'
+import requests from './request'
 
 const Banner = () => {
+
+    const [movie, setMovie] = useState([])
+    useEffect(() => {
+      async function fetchData()  {
+        const request = await axios.get(requests.fetchNetflixOriginals)
+        setMovie(
+            request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+            ]
+        )
+        return request;
+      }
+
+      fetchData()
+    
+    //   return () => {
+    //     second
+    //   }
+    }, [])
+
+    console.log(movie)
+    
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string
